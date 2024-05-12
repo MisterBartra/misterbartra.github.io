1c1fc53d8e2639ae23cd2baaab3f60b9e129867a
MisterBartra <jedubartra13@gmail.com>
2024-05-11 20:51:55 -0500

    Moving files

--- HTML-CoreProyects/∞Iframes/getAndSetMachine.js
+++
@@ -1,9 +0,0 @@
-
-/*const framesMachine = document.getElementById("iframesMachine");
-function getAndSetMachine() {
-	framesMachine.addEventListener("load", function () {
-        document.title = framesMachine.contentDocument.title || "";
-        framesMachine.contentWindow.urlRowIframes = urlRowIframes;
-    }); // Asigna la variable al iframe
-    return window.onload = function() { framesMachine.contentWindow.urlRowIframes = urlRowIframes; }
-}*/
