71562a06d7022e7b8203baff26b21f68fa29d29e
MisterBartra <jedubartra13@gmail.com>
2024-06-03 11:52:40 -0500

    Update

--- HTML-CoreProyects/∞Iframes/loadIframes.js
+++ HTML-CoreProyects/∞Iframes/loadIframes.js
@@ -1,4 +1,3 @@
 function loadIframes() {
     window.location.href=`https://misterbartra.github.io/HTML-CoreProyects/%E2%88%9EIframes/%E2%88%9EIframes.html?urlRowIframes=${encodeURIComponent(JSON.stringify(urlRowIframes))}`;
 }
-loadIframes()
