31717ef9987e1fb1774c6255a795eae6604d502b
MisterBartra <jedubartra13@gmail.com>
2024-06-03 11:44:17 -0500

    Update loadIframes.js

--- HTML-CoreProyects/∞Iframes/loadIframes.js
+++ HTML-CoreProyects/∞Iframes/loadIframes.js
@@ -1,1 +1,4 @@
-window.location.href=`https://misterbartra.github.io/HTML-CoreProyects/%E2%88%9EIframes/%E2%88%9EIframes.html?urlRowIframes=${encodeURIComponent(JSON.stringify(urlRowIframes))}`;
+function loadIframes() {
+    window.location.href=`https://misterbartra.github.io/HTML-CoreProyects/%E2%88%9EIframes/%E2%88%9EIframes.html?urlRowIframes=${encodeURIComponent(JSON.stringify(urlRowIframes))}`;
+}
+loadIframes()
