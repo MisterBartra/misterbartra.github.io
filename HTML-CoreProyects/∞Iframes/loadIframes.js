9ea7dfbd2534be3ac900ec82a5a921aeb81777c3
MisterBartra <jedubartra13@gmail.com>
2024-06-25 01:30:56 -0500

    Add new proyect

--- HTML-CoreProyects/∞Iframes/loadIframes.js
+++
@@ -1,3 +0,0 @@
-function loadIframes() {
-    window.location.href=`https://misterbartra.github.io/HTML-CoreProyects/%E2%88%9EIframes/%E2%88%9EIframes.html?urlRowIframes=${encodeURIComponent(JSON.stringify(urlRowIframes))}`;
-}
