13161637ca443750f6bf3ba0ab62cc162c8cb00c
MisterBartra <jedubartra13@gmail.com>
2024-07-10 19:37:23 -0500

    All Right

---
+++ HTML-Links/Assitants&Tools/app/src/components/component/bundle.js
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client';
+import { FastWebTools } from './fast-web-tools'
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<FastWebTools />, container);
